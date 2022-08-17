4b51549172ac746c9338e87bfe311a4089f81b20
Sam Turner <samapturner@gmail.com>
2022-08-17 22:53:14 +1000

    idk

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 
 Auth::routes(['verify' => true]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('verified');
 
 Route::get('/about',function(){
     return view('about');
