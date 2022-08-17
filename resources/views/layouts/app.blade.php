c607391e9a4b19f0833454ba8af1e27b89459558
Sam Turner <samapturner@gmail.com>
2022-08-18 01:50:29 +1000

    added logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,12 +24,11 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    <img src="{{ asset('logo.jpeg') }}" class="d-inline-block align-middle" width="90" height="90" alt=""> samturner.lol
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
